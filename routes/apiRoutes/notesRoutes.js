af6ea20c8e555b45ed0938bfa0df4cbd06fbdaaf
Scuba Steve <78673754+blacksteven1@users.noreply.github.com>
2021-05-05 12:22:34 +0300

    added routes

---
+++ routes/apiRoutes/notesRoutes.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+const notes = require('../../db/db.json')
+
+router.get('/notes', (req,res) => {
+    let results = notes
+
+    res.json(results)
+    console.log(results.id)
+})
+
+
+
+
+
+module.exports = router;
